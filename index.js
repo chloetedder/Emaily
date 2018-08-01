8f71abb712d75453c530178617a5e2b6f7ddba65
chloetedder <chloe_tedder@yahoo.com>
2018-08-01 17:25:38 -0500

    users can see survey results

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { mongo } from 'mongoose';
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
@@ -8,6 +10,7 @@ require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
